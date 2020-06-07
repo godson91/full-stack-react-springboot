0137e23d46ecf12536ccd73be970963069303648
admin <aborjrcl@gmail.com>
2020-06-07 11:50:05 -0500

    Sent state of increment from child class to parent class

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,23 +3,46 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
  
-
-   class CounterButton extends Component {
-
+class Counter extends Component {
     constructor(){
         super();
         this.state = {
             counter : 0,
-            counter1 : 5,
-            counter2 : 10
         }
+        this.increment = this.increment.bind(this)
+    }
+    render() {
+      return (
+        <div className="counter">
+          <CounterButton incrementMethod={this.increment}/>
+          <CounterButton by={5} incrementMethod={this.increment}/>
+          <CounterButton by={10} incrementMethod={this.increment}/>
+          <span className="count">{this.state.counter}</span>
+        </div>
+      );
+    }
+    increment(by){
+        // console.log(`increment from child  Counterbutton class - ${by}`)
+        this.setState({
+            counter : this.state.counter + by
+        })
+    }
+  }
+   class CounterButton extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            counter : 0,
+        }
+
         this.increment = this.increment.bind(this)
     }
        render () {
            return (
                <div className="Counter">
                    <button onClick={this.increment}>{this.props.by}</button>
-                   <span className="count">{this.state.counter}</span>
                </div>
            );
        }
@@ -28,6 +51,7 @@ import './Counter.css';
             this.setState({
                 counter : this.state.counter + this.props.by
             })
+            this.props.incrementMethod(this.props.by);
     }
 }
 
@@ -41,4 +65,4 @@ CounterButton.propTypes = {
 
 
 
-export default CounterButton;
+export default Counter;
